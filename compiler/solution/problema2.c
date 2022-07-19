53143dc10bc0bd5bf9257a88db944227054cfe93
Janeto <janetoerick18@gmail.com>
2022-07-19 01:53:59 -0300

    return void function resolvido

--- compiler/solution/problema2.c
+++ compiler/solution/problema2.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-double main (    )
+double  i_1 , i_2 , i_3 , i_4 ;  ;
+void  integrar ( double  a)
 {
-	double  a ;    
-	a = 0;    
-	double  i_1 , i_2 , i_3 , i_4 ;    
-	i_1 = 0;    
-	i_2 = 0;    
-	i_3 = 0;    
-	i_4 = 0;    
-	while5:
-	if (!(a >= 0)) goto while5_skip;
-
-	scanf("%lf", &a );   
 	if (!(a >= 0 && a <= 25)) goto if1_skip;
 
 i_1 = i_1 + 1;  
@@ -37,6 +27,21 @@ if (!(a >= 76 && a <= 100)) goto if4_skip;
 i_4 = i_4 + 1;  
 
 	if4_skip:
+
+}
+double main (    )
+{
+	double  a ;    
+	a = 0;    
+	i_1 = 0;    
+	i_2 = 0;    
+	i_3 = 0;    
+	i_4 = 0;    
+	while5:
+	if (!(a >= 0)) goto while5_skip;
+
+	scanf("%lf", &a );   
+integrar ( a ) ;
 
 	goto while5;
 	while5_skip:
